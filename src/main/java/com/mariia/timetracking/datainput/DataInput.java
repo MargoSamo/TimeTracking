d13f742ed533dca84047e3882ff93a59a9123567
MargoSamo <margo_ii@mail.ru>
2022-08-12 11:32:52 +0500

    data input and parse date

--- src/main/java/com/mariia/timetracking/datainput/DataInput.java
+++ src/main/java/com/mariia/timetracking/datainput/DataInput.java
@@ -1,2 +1,22 @@
-package com.mariia.timetracking.dataInput;public class DataInput {
+package com.mariia.timetracking.datainput;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class DataInput {
+
+    public static void main(String[] args) {
+
+        Scanner console = new Scanner(System.in);
+        String name = console.nextLine();
+        try {
+            Date date = new SimpleDateFormat("dd.MM.yyyy").parse(name);
+            System.out.println(date);
+        } catch (ParseException e) {
+            System.out.println("Вы ввели неверный формат даты");
+        }
+
+    }
 }
--- src/main/java/com/mariia/timetracking/datainput/Test.java
+++ src/main/java/com/mariia/timetracking/datainput/Test.java
@@ -1,2 +1,18 @@
-package com.mariia.timetracking.datainput;public class Test {
+package com.mariia.timetracking.datainput;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args) throws ParseException {
+
+        String stringDate = "0121995";
+        try {
+            Date date = new SimpleDateFormat("dd.MM.yyyy").parse(stringDate);
+            System.out.println("Date is : " + date);
+        } catch (ParseException e) {
+            System.out.println("Вы ввели неверный формат даты");
+        }
+    }
 }
