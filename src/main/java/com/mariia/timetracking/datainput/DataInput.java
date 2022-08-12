33685feed4e20effa820851af66aea88178b26d5
MargoSamo <margo_ii@mail.ru>
2022-08-12 13:04:29 +0500

    save dates of memory

--- src/main/java/com/mariia/timetracking/datainput/DataInput.java
+++ src/main/java/com/mariia/timetracking/datainput/DataInput.java
@@ -1,7 +1,10 @@
 package com.mariia.timetracking.datainput;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -9,14 +12,22 @@ public class DataInput {
 
     public static void main(String[] args) {
 
-        Scanner console = new Scanner(System.in);
-        String name = console.nextLine();
-        try {
-            Date date = new SimpleDateFormat("dd.MM.yyyy").parse(name);
-            System.out.println(date);
-        } catch (ParseException e) {
-            System.out.println("Вы ввели неверный формат даты");
-        }
+        ArrayList<Date> dates = new ArrayList<Date>();
 
+        Scanner console = new Scanner(System.in);
+        while(console.hasNext()) {
+        String name = console.nextLine();
+            try {
+                Date newDate = new SimpleDateFormat("dd.MM.yyyy").parse(name);
+                // System.out.println(date);
+                dates.add(newDate);
+                for (Date date : dates) {
+                    System.out.println(date);
+                }
+
+            } catch (ParseException e) {
+                System.out.println("Вы ввели неверный формат даты");
+            }
+        }
     }
 }
